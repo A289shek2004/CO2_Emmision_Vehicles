ea15fe5bdbb3088afd6006e36f26430e5fe4dacf
Abhishek Gupta <abhishekgup2004@gmail.com>
2025-11-27 09:33:17 +0530

    Remove pip install for matplotlib from app.py
    Removed pip install command for matplotlib.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 
 # Importing libraries-----------------------------------------------------------------------------------------
-! pip install matplotlib
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
